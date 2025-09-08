34b6e002c4c779ca5be429ad8ad41f7c15cea73c
Douglas Louteiro <douglas.louterio@protonmail.ch>
2025-09-08 16:54:43 -0300

    feat(Button): adiciona dependência para uso do decorator @override
    - Inclui a dependência necessária para habilitar o uso do @override
    - Sobrescreve métodos relacionados aos eventos de entrada e saída do cursor no widget button.py

--- application/gui/widgets/button.py
+++ application/gui/widgets/button.py
@@ -1,6 +1,17 @@
 from PySide6.QtWidgets import QPushButton
-
+from PySide6.QtCore import Qt
+from typing_extensions import override
 
 class Button(QPushButton):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    @override
+    def enterEvent(self, event):
+        self.setCursor(Qt.CursorShape.PointingHandCursor)
+        super().enterEvent(event)
+
+    @override
+    def leaveEvent(self, event):
+        self.unsetCursor()
+        super().leaveEvent(event)
