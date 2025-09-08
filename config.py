584fa8c4ea5581d1c0252134b738b6a9ff231af0
Douglas Louteiro <douglas.louterio@protonmail.ch>
2025-09-08 16:57:15 -0300

    refactor(config): reestrutura variáveis de ambiente no arquivo de configuração
    - Adiciona novas variáveis de ambiente
    - Atualiza o .env.example para refletir corretamente as mudanças

--- config.py
+++ config.py
@@ -1,12 +1,27 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
 load_dotenv()
 
 
 APP_NAME = os.getenv("APP_NAME", "SYS_PUCPR_GUI")
 APP_VERSION = os.getenv("APP_VERSION", "1.0.0")
-WINDOW_WIDTH = int (os.getenv("WINDOW_WIDTH", 800))
-WINDOW_HEIGHT = int (os.getenv("WINDOW_HEIGHT", 600))
+APP_ROOT_DIR = Path(__file__).resolve().parent
 
-MAIN_STYLE = {"background-color": "#F8F9FA", "color": "#8A0538"}
+# GEOMETRY
+WINDOW_WIDTH = int(os.getenv("WINDOW_WIDTH", 800))
+WINDOW_HEIGHT = int(os.getenv("WINDOW_HEIGHT", 600))
+
+# STYLE
+PRIMARY_COLOR = os.getenv("PRIMARY_COLOR", "#8A0538")
+PRIMARY_COLOR_DARKER = os.getenv("PRIMARY_COLOR_DARKER", "#5A0728")
+
+LIGHT_COLOR = os.getenv("LIGHT_COLOR", "#F8F9FA")
+
+# FILES PATH
+QSS_STYLESHEET_PATH = APP_ROOT_DIR / "application" / "assets" / "styles"
+FONTS_PATH = APP_ROOT_DIR / "application" / "assets" / "fonts"
+
+# IMAGES
+IMAGES_PATH = APP_ROOT_DIR / "application" / "assets" / "images"
