6776ab6f53129a152281592bb260d528f8295b42
Douglas Louteiro <douglas.louterio@protonmail.ch>
2025-09-08 17:04:13 -0300

    feat(layouts): adiciona alinhamento padrão no layout buttons_grid.py

--- application/gui/layouts/buttons_grid.py
+++ application/gui/layouts/buttons_grid.py
@@ -1,18 +1,22 @@
 from PySide6.QtWidgets import QGridLayout
+from PySide6.QtCore import Qt
 from ..widgets.button import Button
 from typing import List
 
 
 class ButtonsGrid(QGridLayout):
-    def __init__(self, items=List[List[str]], *args, **kwargs):
+    def __init__(self, items=List[List[Button]], *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.setAlignment(Qt.AlignmentFlag.AlignBottom)
+
         self._items = items
         self._set_buttons_in_grid()
 
     def _get_grid_greater_column_span(self):
         greatest_column_span = 0
 
-        for column_data in self._items:
+        for _, column_data in enumerate(self._items):
             if len(column_data) > greatest_column_span:
                 greatest_column_span = len(column_data)
 
@@ -22,8 +26,11 @@ class ButtonsGrid(QGridLayout):
         self._get_grid_greater_column_span()
 
         for row, row_data in enumerate(self._items):
-            for col, button_text in enumerate(row_data):
-
-                button = Button(button_text.capitalize())
-                self.addWidget(button, row, col, 1,
-                               1 if len(row_data) > 1 else self._get_grid_greater_column_span())
+            for col, button in enumerate(row_data):
+                self.addWidget(
+                    button,
+                    row,
+                    col,
+                    1,
+                    1 if len(row_data) > 1 else self._get_grid_greater_column_span(),
+                )
