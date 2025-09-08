a87c8bb784e902c1a7580d8653a9f548dd98ed0b
Douglas Louteiro <douglas.louterio@protonmail.ch>
2025-09-08 17:09:10 -0300

    refactor(main_window): reestrutura classe responsável por inicializar a aplicação
    - Substitui central_widget por QStackedWidget para renderizar páginas condicionalmente
    - Remove métodos obsoletos

--- application/gui/views/main_window.py
+++ application/gui/views/main_window.py
@@ -1,29 +1,36 @@
-from config import APP_NAME, WINDOW_WIDTH, WINDOW_HEIGHT, MAIN_STYLE
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QApplication, QLayout
+from typing import Optional
+from config import APP_NAME, WINDOW_WIDTH, WINDOW_HEIGHT, IMAGES_PATH, LIGHT_COLOR
+from PySide6.QtWidgets import (
+    QMainWindow,
+    QStackedWidget,
+    QApplication,
+    QWidget
+)
+
+from .main_menu import MainMenu
+from .actions_menu import ActionsMenu
+from type_defs.menu import MenuOption
 
 
 class MainWindow(QMainWindow):
     def __init__(self, application: QApplication, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        self.main_menu = None
+        self.actions_menu = None
+
         self._setup_window(application)
+        self._render_main_menu()
 
     def _setup_window(self, application: QApplication) -> None:
-        self.setWindowTitle(APP_NAME)
-        self._setup_window_layout()
+        self._set_window_title("Menu Principal")
         self._setup_window_geometry(application)
-        self._set_main_style()
+        self._setup_window_widget()
 
-    def _set_main_style(self):
-        self.setStyleSheet(
-            f"background-color: {MAIN_STYLE['background-color']}; color: {MAIN_STYLE['color']}")
+        self.setStyleSheet(f"background-color:{LIGHT_COLOR}; font-family:Inter;")
 
-    def _setup_window_layout(self):
-        self.window = QMainWindow()
-        self.central_widget = QWidget()
-        self.window_layout = QVBoxLayout()
-        self.central_widget.setLayout(self.window_layout)
-        self.setCentralWidget(self.central_widget)
+    def _setup_window_widget(self):
+        self.stack = QStackedWidget()
+        self.setCentralWidget(self.stack)
 
     def _setup_window_geometry(self, application: QApplication) -> None:
         screen = application.primaryScreen()
@@ -35,8 +42,21 @@ class MainWindow(QMainWindow):
         self.setGeometry(pos_x, pos_y, WINDOW_WIDTH, WINDOW_HEIGHT)
         self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
 
-    def add_widget(self, widget: QWidget, stretch: int = 0, alignment: Qt.AlignmentFlag = Qt.AlignTop | Qt.AlignLeft) -> None:
-        self.central_widget.layout().addWidget(widget, stretch, alignment)
+    def _set_window_title(self, title: Optional[str] = None) -> None:
+        self.setWindowTitle(f"{APP_NAME} - {title}" if title else APP_NAME)
 
-    def add_layout(self, layout: QLayout):
-        self.central_widget.layout().addLayout(layout)
+    def _render_main_menu(self) -> None:
+        self.main_menu = MainMenu(self._render_option_actions_page)
+        self.stack.addWidget(self.main_menu)
+
+    def _go_to_main_menu(self, current_page: QWidget) -> None:
+        self._set_window_title("Menu Principal")
+        self.stack.setCurrentWidget(self.main_menu)
+        self.stack.removeWidget(current_page)
+        current_page.deleteLater()
+
+    def _render_option_actions_page(self, selected_option: MenuOption) -> None:
+        self._set_window_title(selected_option["title"].capitalize())
+        self.actions_menu = ActionsMenu(selected_option['id'], lambda *args: self._go_to_main_menu(self.actions_menu))
+        self.stack.addWidget(self.actions_menu)
+        self.stack.setCurrentWidget(self.actions_menu)
